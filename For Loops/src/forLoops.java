89c166ef0e56efec74c64030c0671d3ea79f8f93
taylor.nielsen <taylor.nielsen@MU-C1-22.mullen.pvt.k12.co.us>
2018-08-27 08:45:52 -0600

    on challenge two

---
+++ For Loops/src/forLoops.java
@@ -0,0 +1,26 @@
+public class forLoops {
+
+	public static void main(String[] args) 
+	{
+		challengeOne();
+		challengeTwo();
+
+	}
+	
+	public static void challengeOne()
+	{
+		for (int i=1; i<=5; i++)
+		{
+			System.out.println(i);
+		}
+	}
+	
+	public static void challengeTwo()
+	{
+		for (int i=2; i<=10; i=i+2)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
